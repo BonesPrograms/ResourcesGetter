69a97869743c605a7b2b06a505efdda57bf2c05a
BonesPrograms <devinbrown0322@gmail.com>
2026-06-22 16:39:20 -0400

    vvv

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Collections.ObjectModel;
+using System.Reflection;
 using BonesClassLibrary.FileFinders;
 using ResourcesGetter;
 
@@ -14,33 +15,31 @@ namespace ResourcesGetter
         static readonly string GameResourcesFolder = Path.Combine(VietnamWarSource.Path, "Vietnam War_Data");
         static readonly string BackupResourcesFolder = Path.Combine(VietnamWarModLab.Path, @"ResourcesGetter\BackupResources");
         static readonly string ModResourcesFolder = Path.Combine(VietnamWarModLab.Path, @"ResourcesGetter\ModResources");
-        static readonly (string, string)[] ModResources = [.. Directory.GetFiles(ModResourcesFolder).Select(file => (Name: Path.GetFileName(file), Path: file))];
-        static readonly string[] ResourceNames = [.. ModResources.Select(file => file.Item1)];
+        static readonly Dictionary<string, string> ModResources = Directory.GetFiles(ModResourcesFolder).Select(file => (Name: Path.GetFileName(file), Path: file)).ToDictionary();
         static readonly Dictionary<string, string> GameResources = GetResources(GameResourcesFolder);
-
         public static void Run()
         {
-            GameResources.ToList().ForEach(file =>
-            {
-                string copyTo = Path.Combine(BackupResourcesFolder, file.Key);
-                Console.WriteLine($"Backuping up {file.Key} to {copyTo}!");
-                File.Copy(file.Value, copyTo, true);
-            });
+            Copy(GameResources, key => Path.Combine(BackupResourcesFolder, key));
+            Copy(ModResources, key => GameResources[key]);
+        }
 
-            ModResources.ToList().ForEach(file =>
+        static void Copy(Dictionary<string, string> obj, Func<string, string> expr)
+        {
+            obj.ToList().ForEach(x =>
             {
-                string copyTo = GameResources[file.Item1];
-                Console.WriteLine($"Copying {file.Item1} to {copyTo}!");
-                File.Copy(file.Item2, copyTo, true);
+                string copyTo = expr(x.Key);
+                Console.WriteLine($"Copying {x.Key} from {x.Value} to {copyTo}!");
+                File.Copy(x.Value, copyTo, true);
             });
         }
         static Dictionary<string, string> GetResources(string path)
         {
-            Dictionary<string, string> resources = new(ResourceNames.Length);
+            string[] names = [.. ModResources.Keys];
+            Dictionary<string, string> resources = new(names.Length);
             foreach (var file in Directory.EnumerateFiles(path, "*.assets", SearchOption.TopDirectoryOnly))
             {
                 string name = Path.GetFileName(file);
-                if (ResourceNames.Contains(name))
+                if (names.Contains(name))
                 {
                     resources[name] = file;
                 }
