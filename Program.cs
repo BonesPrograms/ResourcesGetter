e1052d3b2857b9096b2f1ee1ca54a99f7d5f011f
BonesPrograms <devinbrown0322@gmail.com>
2026-07-02 16:14:31 -0400

    Finalized

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections.ObjectModel;
 using System.Reflection;
-using BonesClassLibrary.FileFinders;
+using BonesClassLibrary.IO;
+using BonesClassLibrary;
 using ResourcesGetter;
 
 
 ResourcesReplacer.Run();
-
 
 namespace ResourcesGetter
 {
@@ -17,13 +17,52 @@ namespace ResourcesGetter
         static readonly string ModResourcesFolder = Path.Combine(VietnamWarModLab.Path, @"ResourcesGetter\ModResources");
         static readonly Dictionary<string, string> ModResources = Directory.GetFiles(ModResourcesFolder).Select(file => (Name: Path.GetFileName(file), Path: file)).ToDictionary();
         static readonly Dictionary<string, string> GameResources = GetResources(GameResourcesFolder);
+        static readonly string[] Options = ["BACKUP", "RESTORE FROM BACKUP", "INSTALL"];
         public static void Run()
         {
-            if (!Directory.EnumerateFiles(BackupResourcesFolder).Any())
-                Copy(GameResources, key => Path.Combine(BackupResourcesFolder, key));
-            Copy(ModResources, key => GameResources[key]);
+            int option = ConsoleHelper.Choices(Options, "This program is for backing up Vietnam War assets and installing mod assets in their place.");
+            if (option == 0)
+            {
+                if (Backup() == false)
+                    Console.WriteLine("Already backed up!");
+            }
+            else if (option == 1)
+            {
+                Console.WriteLine("Restoring from backup...");
+                Restore();
+            }
+            else if (option == 2)
+            {
+                Backup();
+                Console.WriteLine("Installing...");
+                Copy(ModResources, key => GameResources[key]);
+            }
         }
 
+        public static void Restore()
+        {
+            if (!Directory.EnumerateFiles(BackupResourcesFolder).Any())
+                throw new FileNotFoundException("Backup resources folder is empty.");
+            var backups = GetResources(BackupResourcesFolder);
+            Copy(backups, key => Path.Combine(GameResourcesFolder, key));
+        }
+
+        static bool Backup()
+        {
+            if (!Directory.EnumerateFiles(BackupResourcesFolder).Any())
+            {
+                Console.WriteLine("Backing up...");
+                Copy(GameResources, key => Path.Combine(BackupResourcesFolder, key));
+                return true;
+            }
+            return false;
+
+        }
+
+        //here, dictionary obj is the resources you are copying from
+        //expr is a func that creates the copy target
+        //typically it is going to be a Path.Combine with your target folder path + the key of the current object selected in the dictionary
+        //(which will be a filename key w/o a path)
         static void Copy(Dictionary<string, string> obj, Func<string, string> expr)
         {
             obj.ToList().ForEach(x =>
